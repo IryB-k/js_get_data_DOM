c3d64a175ad5bfb38730776d47415a2589f2aca0
Iryna <kapustyak.ira97@gmail.com>
2025-02-08 12:04:23 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+const el = [...document.querySelectorAll('.population')];
+
+const populations = el.map((item) =>
+  Number(item.textContent.replaceAll(',', '')),);
+
+function getTotal() {
+  let total = 0;
+
+  for (let i = 0; i < populations.length; i++) {
+    total += populations[i];
+  }
+
+  return total;
+}
+
+function getAverage() {
+  let sum = 0;
+
+  for (const country of populations) {
+    sum += country;
+  }
+
+  return sum / populations.length;
+}
+
+document.querySelector('.total-population').textContent =
+  getTotal().toLocaleString();
+
+document.querySelector('.average-population').textContent =
+  getAverage().toLocaleString();
